7a207a3237f74f137b5b9568c329f3ec8a7251dd
guilianmanbu <31583809+guilianmanbu@users.noreply.github.com>
2018-03-30 17:33:38 +0800

    tweenner util

---
+++ Tweenner/TweenUnit.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class TweenUnit
+{
+    public int index;
+    public string tag;
+
+    public float startValue;
+    public float endValue;
+
+    public float time;
+    public float interval;
+    public float startTime;
+    public float endTime { get { return time + startTime; } }
+    private float lastUpdateTime;
+    private float deltaTime;
+
+    public bool fixedTime;
+
+    public object hashKey;
+
+    private Action<float> dele;
+    private Action endCallback;
+
+    public void Init(int _index,float _startValue,float _endValue,float _interval, float _time,Action<float> _delegate,Action _endCallback,bool _fixedTime, string _tag)
+    {
+        index = _index;
+        tag = _tag;
+        startValue = _startValue;
+        endValue = _endValue;
+        time = _time;
+        interval = _interval;
+        startTime = _fixedTime ? Time.unscaledTime : Time.time;
+        lastUpdateTime = startTime;
+        deltaTime = 0;
+        fixedTime = _fixedTime;
+        hashKey = _delegate;
+
+        if (_delegate == null)
+            dele = null;
+        else
+            dele = value => { _delegate(Mathf.Lerp(startValue, endValue, value)); };
+
+        endCallback = _endCallback;
+    }
+
+    private Vector3 startV3;
+    private Vector3 endV3;
+    public void Init(int _index, Vector3 _startValue, Vector3 _endValue, float _interval, float _time, Action<Vector3> _delegate, Action _endCallback, bool _fixedTime, string _tag)
+    {
+        index = _index;
+        tag = _tag;
+        startV3 = _startValue;
+        endV3 = _endValue;
+        time = _time;
+        interval = _interval;
+        startTime = _fixedTime ? Time.unscaledTime : Time.time;
+        lastUpdateTime = startTime;
+        deltaTime = 0;
+        fixedTime = _fixedTime;
+        hashKey = _delegate;
+
+        if (_delegate == null)
+            dele = null;
+        else
+            dele = value => { _delegate(Vector3.Lerp(startV3, endV3, value)); };
+
+        endCallback = _endCallback;
+    }
+
+    private Vector2 startV2;
+    private Vector2 endV2;
+    public void Init(int _index, Vector2 _startValue, Vector2 _endValue, float _interval, float _time, Action<Vector2> _delegate, Action _endCallback, bool _fixedTime, string _tag)
+    {
+        index = _index;
+        tag = _tag;
+        startV2 = _startValue;
+        endV2 = _endValue;
+        time = _time;
+        interval = _interval;
+        startTime = _fixedTime ? Time.unscaledTime : Time.time;
+        lastUpdateTime = startTime;
+        deltaTime = 0;
+        fixedTime = _fixedTime;
+        hashKey = _delegate;
+
+        if (_delegate == null)
+            dele = null;
+        else
+            dele = value => { _delegate(Vector2.Lerp(startV2, endV2, value)); };
+
+        endCallback = _endCallback;
+    }
+
+    public bool Update(float currentTime)
+    {
+        if (IsEnd(currentTime))
+        {
+            return true;
+        }
+
+        if (dele != null)
+        {
+            deltaTime += currentTime - lastUpdateTime;
+            lastUpdateTime = currentTime;
+
+            if (deltaTime >= interval)
+            {
+                deltaTime -= interval;
+                float value = (currentTime - startTime) / time;
+                dele(value);
+            }
+        }
+        return false;
+    }
+
+    public bool IsEnd(float currentTime)
+    {
+        return currentTime >= endTime;
+    }
+
+    public void DoEnd()
+    {
+        if (dele != null) dele(1);
+        if (endCallback != null) endCallback();
+    }
+}
+
---
+++ Tweenner/Tweenner.cs
@@ -0,0 +1,214 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Tweenner : MonoBehaviour {
+
+    private static Tweenner instance;
+    public static Tweenner Instance
+    {
+        get
+        {
+            if (instance==null)
+            {
+                GameObject go = new GameObject("Tweenner");
+                DontDestroyOnLoad(go);
+                instance = go.AddComponent<Tweenner>();
+            }
+            return instance;
+        }
+    }
+
+    private Dictionary<int, TweenUnit> units = new Dictionary<int, TweenUnit>();
+    private Dictionary<object, TweenUnit> toDic = new Dictionary<object, TweenUnit>();
+
+    private int index = 0;
+    private List<TweenUnit> endList = new List<TweenUnit>();
+
+    public int To(float startValue,float endValue,float interval,float time,Action<float> _delegate,Action endCallback,bool fixedTime,string tag)
+    {
+        if (_delegate == null) return -1;
+
+        TweenUnit unit;
+        if (toDic.TryGetValue(_delegate,out unit))
+        {
+            unit.Init(unit.index, startValue, endValue, interval, time, _delegate, endCallback, fixedTime, tag);
+        }
+        else
+        {
+            unit = new TweenUnit();
+            unit.Init(NextIndex(), startValue, endValue, interval, time, _delegate, endCallback, fixedTime, tag);
+
+            units.Add(unit.index, unit);
+            toDic.Add(_delegate, unit);
+        }
+
+        return unit.index;
+    }
+
+    public int To(Vector3 startValue, Vector3 endValue, float interval, float time, Action<Vector3> _delegate, Action endCallback, bool fixedTime, string tag)
+    {
+        if (_delegate == null) return -1;
+        
+        TweenUnit unit;
+        if (toDic.TryGetValue(_delegate, out unit))
+        {
+            unit.Init(unit.index, startValue, endValue, interval, time, _delegate, endCallback, fixedTime, tag);
+        }
+        else
+        {
+            unit = new TweenUnit();
+            unit.Init(NextIndex(), startValue, endValue, interval, time, _delegate, endCallback, fixedTime, tag);
+
+            units.Add(unit.index, unit);
+            toDic.Add(_delegate, unit);
+        }
+
+        return unit.index;
+    }
+
+    public int To(float startValue, float endValue, float interval, float time, Action<float> _delegate, Action endCallback, bool fixedTime)
+    {
+        return To(startValue, endValue, interval, time, _delegate, endCallback, fixedTime, string.Empty);
+    }
+    public int To(float startValue, float endValue, float interval, float time, Action<float> _delegate, Action endCallback, string tag)
+    {
+        return To(startValue, endValue, interval, time, _delegate, endCallback, false, tag);
+    }
+    public int To(float startValue, float endValue, float interval, float time, Action<float> _delegate, Action endCallback)
+    {
+        return To(startValue, endValue, interval, time, _delegate, endCallback, false, string.Empty);
+    }
+    public int To(Vector3 startValue, Vector3 endValue, float interval, float time, Action<Vector3> _delegate, Action endCallback)
+    {
+        return To(startValue, endValue, interval, time, _delegate, endCallback, false, string.Empty);
+    }
+
+    public int DelayCall(float time, Action callBack,string tag)
+    {
+        TweenUnit unit = new TweenUnit();
+        unit.Init(NextIndex(), 0, 0, 0, time, null, callBack, false, tag);
+
+        units.Add(unit.index, unit);
+
+        return unit.index;
+    }
+    public int DelayCall(float time,Action callBack)
+    {
+        return DelayCall(time, callBack, string.Empty);
+    }
+
+    public int NextFrameCall(Action callBack)
+    {
+        return DelayCall(0, callBack);
+    }
+
+    public void Remove(int index,bool toEnd)
+    {
+        if (index == -1) return;
+
+        TweenUnit unit;
+        if (units.TryGetValue(index,out unit))
+        {
+            Remove(unit);
+
+            if (toEnd)
+            {
+                unit.DoEnd();
+            }
+        }
+    }
+
+    public void RemoveByTag(string tag,bool toEnd)
+    {
+        List<TweenUnit> tempList = new List<TweenUnit>();
+        var iterator = units.GetEnumerator();
+        while (iterator.MoveNext())
+        {
+            TweenUnit unit = iterator.Current.Value;
+            if (unit.tag.Equals(tag))
+            {
+                tempList.Add(unit);
+            }
+        }
+
+        if (tempList.Count>0)
+        {
+            for (int i = 0; i < tempList.Count; i++)
+            {
+                TweenUnit unit = tempList[i];
+
+                Remove(unit);
+
+                if(toEnd) unit.DoEnd();
+            }
+        }
+    }
+
+    public void RemoveAll(bool toEnd)
+    {
+        if (toEnd)
+        {
+            Dictionary<int, TweenUnit> tempDic = units;
+
+            units = new Dictionary<int, TweenUnit>();
+            toDic = new Dictionary<object, TweenUnit>();
+            
+            var iterator = tempDic.GetEnumerator();
+            while (iterator.MoveNext())
+            {
+                iterator.Current.Value.DoEnd();
+            }
+        }
+        else
+        {
+            units.Clear();
+            toDic.Clear();
+        }
+    }
+
+    private void Remove(TweenUnit unit)
+    {
+        units.Remove(unit.index);
+        if (unit.hashKey != null) toDic.Remove(unit.hashKey);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (units.Count>0)
+        {
+            float nowTime = Time.time;
+            float nowTimeFixed = Time.unscaledTime;
+
+            var iterator = units.GetEnumerator();
+            while (iterator.MoveNext())
+            {
+                TweenUnit unit = iterator.Current.Value;
+
+                float tempTime = unit.fixedTime ? nowTimeFixed : nowTime;
+
+                bool end = unit.Update(tempTime);
+
+                if (end) endList.Add(unit);
+            }
+
+            if (endList.Count>0)
+            {
+                for (int i = 0; i < endList.Count; i++)
+                {
+                    Remove(endList[i]);
+
+                    endList[i].DoEnd();
+                }
+
+                endList.Clear();
+            }
+        }
+	}
+
+    int NextIndex()
+    {
+        return index++;
+    }
+}
